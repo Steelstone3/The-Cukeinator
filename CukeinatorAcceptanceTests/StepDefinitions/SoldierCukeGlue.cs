1bd7bdf093f952e6926c5743c744122948cfa3cc
Skippy <steelstone3@gmail.com>
2022-10-27 16:25:49 +0100

    Step definitions pencilled in

---
+++ CukeinatorAcceptanceTests/StepDefinitions/SoldierCukeGlue.cs
@@ -0,0 +1,26 @@
+
+using TechTalk.SpecFlow;
+
+namespace CukeinatorAcceptanceTests.StepDefinitions
+{
+    public class SoldierCukeGlue
+    {
+        [Given(@"(.*) soldier ")]
+        public void bob()
+        {
+
+        }
+
+        [When("soldier ")]
+        public void bob2()
+        {
+
+        }
+
+        [Then("soldier ")]
+        public void bob3()
+        {
+
+        }
+    }
+}
